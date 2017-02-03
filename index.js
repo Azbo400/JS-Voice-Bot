3192b2c343f98f977d7a5bc53f5edb2c93252703
azbo400 <azbo400@gmail.com>
2017-02-02 16:05:27 -0800

    Worked on speech recognition

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-//Javascript linked with index.html! 
+//Javascript linked with index.html!
+'use-strict';
+function testher(text){
+
+  if (text === "hi"){
+    responsiveVoice.setDefaultVoice("US English Female");
+    responsiveVoice.speak("Hello there, How may I assist you today?");
+  }
+}
+
+if (annyang) {
+  //Commands
+  var commands = {
+    'hi': function() {
+      responsiveVoice.speak("Hi there, how are you?");
+      console.log('The boy said hi!!!!!!!!!!!!');
+    }
+  };
+
+  // Add our commands to annyang
+  annyang.addCommands(commands);
+
+  // Start listening. You can call this here, or attach this call to an event, button, etc.
+  annyang.start();
+
+  console.log(annyang);
+}
