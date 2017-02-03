b4e45e10737c7a51dbb8d31b287bf791f4a7f63f
azbo400 <azbo400@gmail.com>
2017-02-02 16:24:44 -0800

    Working on weather command

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 //Javascript linked with index.html!
 'use-strict';
-function testher(text){
 
-  if (text === "hi"){
-    responsiveVoice.setDefaultVoice("US English Female");
-    responsiveVoice.speak("Hello there, How may I assist you today?");
-  }
-}
+responsiveVoice.setDefaultVoice("US English Female");
+
 
 if (annyang) {
   //Commands
   var commands = {
     'hi': function() {
-      responsiveVoice.speak("Hi there, how are you?");
-      console.log('The boy said hi!!!!!!!!!!!!');
+      responsiveVoice.speak("Hi there, may I assist you");
+    }
+    'What can you do':function(){
+      responsiveVoice.speak("Here is a list of things I can respond too");
+    }
+    'What is the weather like':function(){
+      fetch('https://ipinfo.io')
+        .then(function(blob){
+          return blob.json();
+        })
+        .then(function(data){
+          console.log(data);
+        });
+
     }
   };
 
