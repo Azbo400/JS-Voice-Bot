2470f7565d8d59735e754125c4afe11325ef464d
azbo400 <azbo400@gmail.com>
2017-02-02 20:49:23 -0800

    stuff

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 responsiveVoice.setDefaultVoice("US English Female");
 
-function weather(){
-  console.log("works");
-}
-
-
 if (annyang) {
-  //Commands
+  // Let's define our first command. First the text we expect, and then the function it should call
   var commands = {
-    'hi': function() {
-      responsiveVoice.speak("Hi there, may I assist you");
+    'hi hello': function() {
+      responsiveVoice.speak("Hello there, How may I assist you?");
     }
   };
-
 
   // Add our commands to annyang
   annyang.addCommands(commands);
 
   // Start listening. You can call this here, or attach this call to an event, button, etc.
   annyang.start();
-
-  console.log(annyang);
 }
