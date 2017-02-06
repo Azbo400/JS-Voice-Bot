b44bb58c856d3fe5953ab0de7cd7f34cb9ba2123
azbo400 <azbo400@gmail.com>
2017-02-06 08:46:33 -0800

    Added browser sync

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ responsiveVoice.setDefaultVoice("US English Female");
 if (annyang) {
   // Let's define our first command. First the text we expect, and then the function it should call
   var commands = {
-    'hi hello': function() {
+    'hi': function() {
       responsiveVoice.speak("Hello there, How may I assist you?");
+      console.log("He said hi");
     }
   };
 
@@ -14,3 +15,4 @@ if (annyang) {
   // Start listening. You can call this here, or attach this call to an event, button, etc.
   annyang.start();
 }
+console.log("works");
